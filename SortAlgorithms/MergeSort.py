35f613b1ad9b4221c1f8faea25c2f4cb4fbe76ae
iddiddavy <86935409+iddiddavy@users.noreply.github.com>
2026-02-24 00:03:12 +0700

    Update MergeSort.py

--- SortAlgorithms/MergeSort.py
+++ SortAlgorithms/MergeSort.py
@@ -1,40 +1,5 @@
 import time
-
-def merge(a,l,mid,r):
-    n1 = mid - l + 1
-    n2 = r - mid
-    L = [0] * n1
-    R = [0] * n2
-    for i in range(n1):
-        L[i] = a[l + i]
-    for i in range(n2):
-        R[i] = a[mid + i + 1]
-    i = 0
-    j = 0
-    k = l
-    while (i < n1 and j < n2):
-        if (L[i] <= R[j]):
-            a[k] = L[i]
-            i = i + 1
-        else:
-            a[k] = R[j]
-            j = j + 1
-        k = k + 1
-    while (i < n1):
-        a[k] = L[i]
-        i = i + 1
-        k = k + 1
-    while (j < n2):
-        a[k] = R[j]
-        j = j + 1
-        k = k + 1
-
-def MergeSort(a,l,r):
-    if (l < r):
-        mid = (l + r) // 2
-        MergeSort(a,l,mid)
-        MergeSort(a,mid + 1,r)
-        merge(a,l,mid,r)
+import numpy as np
 
 avg = 0
 for i in range(1,11):
@@ -44,11 +9,11 @@ for i in range(1,11):
     s = f.readline()
     a = []
     if (i <= 5):
-        a = list(map(float,s.split()))
+        a = np.array(list(map(float,s.split())), dtype=np.float32)
     else:
-        a = list(map(int,s.split()))
+        a = np.array(list(map(int,s.split())), dtype=np.int32)
     start = time.perf_counter()
-    MergeSort(a,0,len(a) - 1)
+    a = np.sort(a,kind = 'mergesort')
     elapsed = time.perf_counter()
     f.close()
     f = open(file_name + ".out",mode = 'w')
