4fc91f85b8968128605aebb21e16df6706c494a5
iddiddavy <86935409+iddiddavy@users.noreply.github.com>
2026-02-24 00:02:51 +0700

    Update HeapSort.py

--- SortAlgorithms/HeapSort.py
+++ SortAlgorithms/HeapSort.py
@@ -1,27 +1,5 @@
 import time
-
-def heapify(a,n,i):
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-    if (l < n and a[l] > a[largest]):
-        largest = l
-    if (r < n and a[r] > a[largest]):
-        largest = r
-    if (largest != i):
-        tam = a[largest]
-        a[largest] = a[i]
-        a[i] = tam
-        heapify(a,n,largest)
-
-def HeapSort(a,n):
-    for i in range(n // 2 - 1,-1,-1):
-        heapify(a,n,i)
-    for i in range(n - 1,-1,-1):
-        tam = a[0]
-        a[0] = a[i]
-        a[i] = tam
-        heapify(a,i,0)
+import numpy as np
 
 avg = 0
 for i in range(1,11):
@@ -31,11 +9,11 @@ for i in range(1,11):
     s = f.readline()
     a = []
     if (i <= 5):
-        a = list(map(float,s.split()))
+        a = np.array(list(map(float,s.split())), dtype=np.float32)
     else:
-        a = list(map(int,s.split()))
+        a = np.array(list(map(int,s.split())), dtype=np.int32)
     start = time.perf_counter()
-    HeapSort(a,len(a))
+    a = np.sort(a,kind = 'heapsort')
     elapsed = time.perf_counter()
     f.close()
     f = open(file_name + ".out",mode = 'w')
